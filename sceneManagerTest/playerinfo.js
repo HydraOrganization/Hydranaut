857f16cc1905745d289d34a40490b65fe21ab054
ChemaSalazar <chemasalazar@outlook.com>
2018-04-20 05:29:33 -0500

    new bg added, landing page modified, user data-bind included

--- sceneManagerTest/playerinfo.js
+++ sceneManagerTest/playerinfo.js
@@ -6,7 +6,16 @@ function parseURLParams() {
     var url = document.location.href;
         queryStart = url.indexOf("?user=") + 6,
         queryEnd   = url.indexOf("&world=") ,
-        playerName = url.slice(queryStart, queryEnd),
+        playerName = url.slice(queryStart, queryEnd);
+        console.log("[debug]: Current URL =>"+url);
+
+        //check to see if the registration part was bypassed. If so, then just printout a generic Guest message.
+        if(url == "http://localhost:3000//sceneManagerTest/index.html" || url == "https://hydranaut.unt.edu/sceneManagerTest/index.html"|| url == "http://hydranaut.unt.edu/sceneManagerTest/index.html"){
+          document.getElementById('playername').innerHTML = "Welcome, Guest (Not logged in)";
+          return;
+        }
+
+
         console.log("Welcome, " + playerName);
 
         //inject the data into the element.
