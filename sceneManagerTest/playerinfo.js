2f4e9e9f449eda9fb91a4930d277d1c6baa93e5b
ChemaSalazar <chemasalazar@outlook.com>
2018-04-20 20:04:02 -0500

    fixed game landing page, added background to worlds

--- sceneManagerTest/playerinfo.js
+++ sceneManagerTest/playerinfo.js
@@ -10,7 +10,7 @@ function parseURLParams() {
         console.log("[debug]: Current URL =>"+url);
 
         //check to see if the registration part was bypassed. If so, then just printout a generic Guest message.
-        if(url == "http://localhost:3000//sceneManagerTest/index.html" || url == "https://hydranaut.unt.edu/sceneManagerTest/index.html"|| url == "http://hydranaut.unt.edu/sceneManagerTest/index.html"){
+        if(url == "http://localhost:3000//sceneManagerTest/index.html" || url == "https://hydranaut.unt.edu/sceneManagerTest/index.html"|| url == "http://hydranaut.unt.edu/sceneManagerTest/index.html" || url == "http://127.0.0.1:3000/sceneManagerTest/"){
           document.getElementById('playername').innerHTML = "Welcome, Guest (Not logged in)";
           return;
         }
